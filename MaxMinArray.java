006783087e4a180159ad0ba7a3874ded7f104259
Sai Bhavana V <saibhavanaravi@gmail.com>
2026-03-04 22:09:32 +0530

    Create MaxMinArray.java

---
+++ MaxMinArray.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class MaxMinArray{
+  public static void main(String[] args){
+    int[] arr = {5,2,8,1,3};
+
+ Arrays.sort(arr);
+ int min = arr[0];
+ int max = arr[arr.length - 1];
+ System.out.println("Minimum element :" +min);
+ System.out.println("Maximum element:" +max);
+
+}
+} 
