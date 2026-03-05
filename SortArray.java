e6c6f4a76d1730f7c36d1f82dfbcc87fa05b177e
Sai Bhavana V <saibhavanaravi@gmail.com>
2026-03-05 10:37:33 +0530

    Create SortArray.java

---
+++ SortArray.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class SortArray{
+
+  public static void main(String[] args){
+   
+  int[] arr = {5,2,8,1,3};
+
+  Arrays.sort(arr);
+
+  System.out.println("Sorted Array:");
+ for(int num: arr){
+    System.out.println(num + " ");
+  }
+
+  }
+
+
+}
